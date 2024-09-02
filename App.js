1e3e5331eff217b6aba243177e89f6881db14726
Sirma2k2 <miro.pennanen@gmail.com>
2024-09-02 16:20:49 +0300

    Valmis viikko 1

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+const [ages, setAges] = useState('')
+const [limits, setLimits] = useState({lower:'', upper:''})
+
+  const Calc = () => {
+    const lower =(220-ages)*0.65
+    const upper = (220-ages)*0.85
+    setLimits({lower: lower.toFixed(), upper: upper.toFixed()})
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.field}>Age</Text>
+      <TextInput
+      style={styles.field}
+      placeholder='gfgfgf'
+      value={ages}
+      onChangeText={text => setAges(text)}
+       keyboardType='decimal-pad'
+      />
+      <Text style={styles.field}>Limits</Text>
+      <Text style={styles.field}>
+        {limits.lower} - {limits.upper}
+      </Text>
+      <Button title='Calculate' onPress={Calc}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +37,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    paddingTop: 20,
+    margin: 8, 
   },
+  field:{
+    marginTop: 8, 
+    marginBottom: 8, 
+  }
 });
